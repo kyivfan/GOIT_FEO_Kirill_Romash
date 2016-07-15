94a0a47d2dc210786d7662a992f1f0506ade3abd
Kirill Romash <kirillromash84@gmail.com>
2016-07-15 14:04:57 +0300

    changed HT JS 1-2

--- proj js/js1/js/script2.js
+++ proj js/js1/js/script2.js
@@ -4,7 +4,7 @@ var namesArray = [];
 function createArray() {
 
     for (var i = 0; i < namesQuantity; i++){
-        namesArray [i] = prompt ('Введите имя номер ' + (i+1) );
+        namesArray[i] = prompt ('Введите имя номер ' + (i + 1) );
     }
     return namesArray;
 }
@@ -13,10 +13,18 @@ function searchName() {
 
     var name = prompt('Введите имя пользователя');
 
-    for (var i = 0; i < namesArray.length; i++) {
-        if (namesArray[i] === name) {
-            return alert(name + ', вы успешно вошли');
-        }
-    }
-    return alert('Ошибка! Имя пользователя не найдено!');
+    namesArray.includes(name) ?
+        alert( name + ', вы успешно вошли') : alert('Ошибка! Имя пользователя не найдено!');
 }
+// ALTERNATIVE SOLUTION
+// function searchName() {
+//
+//     var name = prompt('Введите имя пользователя');
+//
+//     for (var i = 0; i < namesArray.length; i++) {
+//         if (namesArray[i] === name) {
+//             return alert(name + ', вы успешно вошли');
+//         }
+//     }
+//     return alert('Ошибка! Имя пользователя не найдено!');
+// }
